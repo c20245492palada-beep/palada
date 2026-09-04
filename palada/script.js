23f00295224c024cbc2b2c7007af9003c05d2af6
c20245492palada-beep <c20245492.palada@csav.edu.ph>
2026-09-04 14:44:16 +0800

    Variale (let and const)

--- palada/script.js
+++ palada/script.js
@@ -1,2 +1,14 @@
 console.log ("Hello World!");
 console.log ("From script.js file.");
+
+const myName = "Alex";
+let age = 23;
+Age = 24;
+const PhonwNumber = "09302324013"; ;
+const address = "malinong gahod";
+
+console.log(`my name: ${myName}1`);
+console.log(`age: ${age} `);
+console.log(`number: ${Number}`);   
+console.log(`address: ${address}`);
+console.log(67);
